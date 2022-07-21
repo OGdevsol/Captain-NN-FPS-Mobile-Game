20c04cfd07bc01651f9bcf245ddc8e3d46b8acef
Umar Amin <79603472+OGdevsol@users.noreply.github.com>
2022-07-22 01:06:55 +0500

    Add files via upload

--- Level_Spawn_Manager.cs
+++ Level_Spawn_Manager.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -10,6 +11,8 @@ public class Level_Spawn_Manager : MonoBehaviour
 	public GameObject[] enemiesVariantsPrefabs;
 	public Level[] level;
 	private int x;
+	public TMP_Text waveObjectiveGameObject;
+
 
 	private int
 		_currentWaveInLoop; // To only check and instantiate according to the total enemies kept in each wave while initializing enemy instantiation
@@ -24,12 +27,12 @@ public class Level_Spawn_Manager : MonoBehaviour
 
 	private void Start()
 	{
-		InitializeEnemies(); // All enemies will be instantiated and their game objects + transforms will be assigned to their respective waves
-		EnableOnlyCurrentWave(); // All enemies will be deactivated and only those which are in "Current Wave" will be kept active
+		AddWavesToList();
+		InitializeEnemies();
 	}
 
 	private void
-		InitializeEnemies() // Method to initialize enemies in form of waves.  // All enemies will be instantiated and their game objects + transforms will be assigned to their respective waves
+		AddWavesToList() // Method to initialize enemies in form of waves. When all enemies in the first wave are killed in a level, the second wave will be initiated. Level in completed when all waves are killed
 	{
 		var wavesLengthInLevel = level[0].waves.Length;
 		int i;
@@ -37,51 +40,42 @@ public class Level_Spawn_Manager : MonoBehaviour
 		{
 			_currentWaveInLoop = i;
 			level[0].wavesInLevel.Add(level[0].waves[i]);
-			Debug.Log("Instantiating Wave " + _currentWaveInLoop);
-			for (int j = 0; j < level[0].wavesInLevel[_currentWaveInLoop].enemyType.Length; j++)
-			{
-				var E = Instantiate(enemiesVariantsPrefabs[CheckEnemiesType(j)],
-					level[0].waves[_currentWaveInLoop].enemyPosition[j].position,
-					level[0].waves[_currentWaveInLoop].enemyPosition[j].rotation);
-				level[0].totalEnemies.Add(E.transform);
-				level[0].waves[_currentWaveInLoop].enemiesGameObjectInWave.Add(E.gameObject);
-			}
 		}
 	}
 
-	private void
-		EnableOnlyCurrentWave() // A method to check which wave is currently active and how many enemies are left in said wave before initializing next wave. All enemies will be deactivated and only those which are in "Current Wave" will be kept active
+	private void InitializeEnemies()
 	{
-		_currentWaveToKeepActiveIndex = 0;
-		int i;
-		int j;
-		int wavesInLevelCount = level[0].wavesInLevel.Count;
-
-		for (i = 0; i < wavesInLevelCount; i++)
+		waveObjectiveGameObject.text = level[0].wavesInLevel[_currentWaveToKeepActiveIndex].waveObjective;
+		for (int j = 0; j < level[0].wavesInLevel[_currentWaveToKeepActiveIndex].enemyType.Length; j++)
 		{
-			for (j = 0; j < level[0].wavesInLevel[i].enemyType.Length; j++)
-			{
-				level[0].wavesInLevel[i].enemiesGameObjectInWave[j].SetActive(false);
-				level[0].wavesInLevel[_currentWaveToKeepActiveIndex].enemiesGameObjectInWave[j].SetActive(true);
-			}
+			var E = Instantiate(enemiesVariantsPrefabs[CheckEnemiesType(j)],
+				level[0].wavesInLevel[_currentWaveToKeepActiveIndex].enemyPosition[j].position,
+				level[0].waves[_currentWaveToKeepActiveIndex].enemyPosition[j].rotation);
+			level[0].wavesInLevel[_currentWaveToKeepActiveIndex].enemiesGameObjectInWave.Add(E.transform);
 		}
 	}
+
 
 	private static int waveToBeRemovedIndex = 0;
 
 	public void
 		CheckEnemiesInActiveWave() // When there are zero enemies left in currently active wave, next enemy wave will be initialized (Also being used in health script of the AI in AI Kit in KillAI method. NOTE: Don't forget to remove the enemy gameobject from respective wave when it is killed 
 	{
-		if (level[0].wavesInLevel[_currentWaveToKeepActiveIndex].enemiesGameObjectInWave.Count == 0)
+		if (level[0].wavesInLevel[0].enemiesGameObjectInWave.Count == 0)
 		{
 			level[0].wavesInLevel
 				.RemoveAt(waveToBeRemovedIndex); // Wave with zero enemies will be removed/deactivated to initialize next wave and the next wave will take its place at waveToBeRemovedIndex. The cycle will repeat when 0 enemies are left in new wave.
-			EnableOnlyCurrentWave();
+
+			if (level[0].wavesInLevel.Count > 0)
+			{
+				InitializeEnemies();
+			}
 		}
 
 		if (level[0].wavesInLevel.Count == 0)
 		{
 			Debug.Log("LEVEL COMPLETED");
+			waveObjectiveGameObject.text = "Level Completed";
 		}
 	}
 
@@ -89,7 +83,7 @@ public class Level_Spawn_Manager : MonoBehaviour
 	private int
 		CheckEnemiesType(int i) // Method to check which enemy type is selected in the editor in a level wave so THAT particular enemy can be instantiated in its respective position using InitializeEnemies() method
 	{
-		switch (level[0].waves[_currentWaveInLoop].enemyType[i])
+		switch (level[0].wavesInLevel[_currentWaveToKeepActiveIndex].enemyType[i])
 		{
 			case EnemyType.AlienSoldier:
 				x = 0;
@@ -97,11 +91,14 @@ public class Level_Spawn_Manager : MonoBehaviour
 			case EnemyType.TurnedSoldier:
 				x = 1;
 				return x;
-			case EnemyType.AlienPlant:
+			case EnemyType.Mutant:
 				x = 2;
 				return x;
-			case EnemyType.AlienBoss:
+			case EnemyType.AlienPlant:
 				x = 3;
+				return x;
+			case EnemyType.AlienBoss:
+				x = 4;
 				return x;
 			default:
 				return x;
@@ -113,6 +110,7 @@ public class Level_Spawn_Manager : MonoBehaviour
 	{
 		AlienSoldier,
 		TurnedSoldier,
+		Mutant,
 		AlienPlant,
 		AlienBoss
 	}
@@ -121,10 +119,11 @@ public class Level_Spawn_Manager : MonoBehaviour
 [Serializable]
 public class Wave
 {
+	public string waveObjective;
 	public Level_Spawn_Manager.EnemyType[] enemyType;
 	public Transform[] enemyPosition;
 
-	[HideInInspector] public List<GameObject>
+	[HideInInspector] public List<Transform>
 		enemiesGameObjectInWave; // Each level's waves' enemies gameobjects will be placed in this list according to their waves placement. 
 }
 
@@ -133,8 +132,6 @@ public class Level
 {
 	public Wave[] waves; // Add Enemies Details in this array
 
-	[HideInInspector] public List<Transform>
-		totalEnemies; //Total enemies in each level will be added to this list to easily manage their in-game/level functionality
 
 	[HideInInspector] public List<Wave>
 		wavesInLevel; //Total waves in each level will be added to this list to add and maintain functionality control over each wave's properties
